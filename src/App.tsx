8833562a8e2db35bce66a36d72031e26aa49a7bb
Pau Torrents <pau.torrents@thoughtworks.com>
2020-06-13 11:30:42 +0200

    Style Login Page

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,32 @@ import React from 'react'
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
 import 'fontsource-roboto'
 
+
 import LoginPage from './pages/LoginPage/LoginPage'
 import SensorsPage from './pages/SensorsPage/SensorsPage'
+import { createMuiTheme, CssBaseline, MuiThemeProvider } from '@material-ui/core'
+
+const theme = createMuiTheme({
+  palette: {
+    background: {
+      default: "rgba(0, 0, 0, 0.04)"
+    }
+  }
+});
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/login" component={LoginPage} />
-        <Route exact path="/sensors" component={SensorsPage} />
-        <Redirect to="/sensors" />
-      </Switch>
-    </BrowserRouter>
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/sensors" component={SensorsPage} />
+          <Redirect to="/sensors" />
+        </Switch>
+      </BrowserRouter>
+    </MuiThemeProvider>
   )
 }
 
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,11 +1,35 @@
 import React from 'react'
-import { Button, Card, CardActions, CardContent, Typography } from '@material-ui/core'
+import {
+  Button,
+  Paper,
+  PropTypes,
+  TextField,
+  Typography
+} from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles({
   root: {
     marginTop: 50,
-    width: 800
+    width: 600,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    padding: 30,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  loginForm: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginTop: 40,
+    alignSelf: 'stretch'
+  },
+  passwordInput: {
+    marginTop: 30
+  },
+  signInButton: {
+    marginTop: 50
   }
 }, { name: 'LoginPage' })
 
@@ -13,16 +37,19 @@ const LoginPage = () => {
   const classes = useStyles()
 
   return (
-    <div className={classes.root}>
-      <Card>
-        <CardContent>
-          <Typography>Welcome to ThermoCo!</Typography>
-        </CardContent>
-        <CardActions>
-          <Button>Sign in</Button>
-        </CardActions>
-      </Card>
-    </div>
+    <Paper className={classes.root} elevation={3}>
+      <Typography variant="h4" align="center">Welcome to ThermoCo!</Typography>
+      <form className={classes.loginForm}>
+        <TextField id="login-username" label="Username" variant="outlined" />
+        <TextField className={classes.passwordInput} id="login-password" label="Password" variant="outlined" />
+      </form>
+      <Button
+        className={classes.signInButton}
+        color="primary"
+        size="large"
+        variant="contained"
+      >Sign in</Button>
+    </Paper>
   )
 }
 
