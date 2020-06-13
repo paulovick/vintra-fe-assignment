bc8b1064abe71a3b3ef8af1d694b7302bbaa8283
Pau Torrents <pau.torrents@thoughtworks.com>
2020-06-13 11:13:19 +0200

    Add Material-UI library

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { BrowserRouter, Route, Redirect } from 'react-router-dom'
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
+import 'fontsource-roboto'
 
 import LoginPage from './pages/LoginPage/LoginPage'
 import SensorsPage from './pages/SensorsPage/SensorsPage'
@@ -7,9 +8,11 @@ import SensorsPage from './pages/SensorsPage/SensorsPage'
 const App = () => {
   return (
     <BrowserRouter>
-      <Route path="/login" component={LoginPage} />
-      <Route path="/sensors" component={SensorsPage} />
-      <Route path="/" component={() => <Redirect to="/sensors" />} />
+      <Switch>
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/sensors" component={SensorsPage} />
+        <Redirect to="/sensors" />
+      </Switch>
     </BrowserRouter>
   )
 }
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import { Button, Card, CardActions, CardContent, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
 
-const LoginPage = () => (
-  <div>Login page</div>
-)
+const useStyles = makeStyles({
+  root: {
+    marginTop: 50,
+    width: 800
+  }
+}, { name: 'LoginPage' })
+
+const LoginPage = () => {
+  const classes = useStyles()
+
+  return (
+    <div className={classes.root}>
+      <Card>
+        <CardContent>
+          <Typography>Welcome to ThermoCo!</Typography>
+        </CardContent>
+        <CardActions>
+          <Button>Sign in</Button>
+        </CardActions>
+      </Card>
+    </div>
+  )
+}
 
 export default LoginPage
